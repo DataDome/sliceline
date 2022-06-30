b5b85b6d16056835dafbc10b8a37d04f492a2c74
adedaran <antoine.dedaran@datadome.co>
2022-06-30 17:44:03 +0200

    Fix import

--- sliceline/slicefinder.py
+++ sliceline/slicefinder.py
@@ -70,9 +70,8 @@ class Slicefinder(BaseEstimator, TransformerMixin):
 
     References
     ----------
-    `"SliceLine: Fast, Linear-Algebra-based Slice Finding for ML Model Debugging",
-    from Svetlana Sagadeeva and Matthias Boehm of Graz University of Technology.
-    <https://mboehm7.github.io/resources/sigmod2021b_sliceline.pdf>`
+    `SliceLine: Fast, Linear-Algebra-based Slice Finding for ML Model Debugging <https://mboehm7.github.io/resources/sigmod2021b_sliceline.pdf>`,
+    from *Svetlana Sagadeeva* and *Matthias Boehm* of Graz University of Technology.
     """
 
     def __init__(
