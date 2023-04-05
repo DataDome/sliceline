e073217dc3a345b96cdb7e507c85482aa0aa4c28
Antoine de Daran <92319416+adedaran@users.noreply.github.com>
2023-04-05 16:57:47 +0200

    Feature: Lint code (#35)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,37 +1,36 @@
 # Configuration file for the Sphinx documentation builder.
-import sys
 import os
-
+import sys
 
 sys.path.insert(0, os.path.abspath("../../"))
 
 # -- Project information
 
-project = 'Sliceline'
-copyright = '2022, DataDome'
-author = 'Antoine de Daran'
+project = "Sliceline"
+copyright = "2022, DataDome"
+author = "Antoine de Daran"
 
 # -- General configuration
 
 extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
+    "sphinx.ext.duration",
+    "sphinx.ext.doctest",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",
 ]
 
 intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+    "python": ("https://docs.python.org/3/", None),
+    "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
 }
-intersphinx_disabled_domains = ['std']
+intersphinx_disabled_domains = ["std"]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # -- Options for HTML output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # -- Options for EPUB output
-epub_show_urls = 'footnote'
+epub_show_urls = "footnote"
